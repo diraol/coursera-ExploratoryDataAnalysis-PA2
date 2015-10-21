a8215ff03c9462dbb375bd51ac2e94267ad50085
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2015-10-21 08:30:15 -0200

    Adding partial solution to Question 2

--- pa2.R
+++ pa2.R
@@ -49,3 +49,29 @@ barplot(plot1_data$emissions, plot1_data$year, names.arg=plot1_data$year)
 
 # Based on the plotted graph, we can say that the Emissions of PM2.5 on US,
 # from all sources, have decreased from 1999 to 2008.
+
+
+#### PLOT 2 #####
+# Question: Have total emissions from PM2.5 decreased in the Baltimore City,
+# Maryland (fips == "24510") from 1999 to 2008? Use the base plotting system
+# to make a plot answering this question.
+
+### Evaluating PM25 by year in Baltimore City
+# Calculating the sum of emissions of PM25 by year
+plot2_data <- base_data %>%
+    # First filter data to Baltimore (fips=='24510')
+    filter(fips=="24510") %>%
+    # Then select only Pollutant, Emissions and year columns
+    select(Pollutant, Emissions, year) %>%
+    # Now filter by Pollutant equal to PM25-PRI
+    filter(Pollutant=="PM25-PRI") %>%
+    # Grouping data by year
+    group_by(year) %>%
+    # Summarizing data by year as the sum of all data from that year
+    summarise(emissions = sum(Emissions))
+
+# Ploting the Emissions by year:
+barplot(plot2_data$emissions, plot2_data$year, names.arg=plot2_data$year)
+
+# Based on the plotted graph, we can say that the Emissions of PM2.5 on US,
+# from all sources, have decreased from 1999 to 2008.
